40750d7f020b873abc83a21c2f645fadfbc786cb
briannarelucio <briannarelucio@ge.com>
2018-06-29 10:43:27 -0700

    Install CF cli

--- predix_cli.sh
+++ predix_cli.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -L "https://packages.cloudfoundry.org/stable?release=macosx64-binary&source=github" | tar -zx && mv cf /usr/local/bin && cf --version
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && brew tap cloudfoundry/tap && brew install cf-cli
