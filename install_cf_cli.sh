21b9866a61b49e0beca6dc6cd03c7c1aa606a3b7
briannarelucio <briannarelucio@ge.com>
2018-07-03 15:44:52 -0700

    CLI

--- install_cf_cli.sh
+++ install_cf_cli.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)" && brew tap cloudfoundry/tap && brew install cf-cli
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+sudo apt-get update
+sudo apt-get install cf-cli
+sudo wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundry.org/fedora/cloudfoundry-cli.repo
+sudo yum install cf-cli
--- statictest.sh
+++ statictest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ruby statictest.rb
-sed -e "s/$COLOR/silver/g" ./app/views/layouts/application.html.erb
+sed -e "s/COLOR_REPLACE_ME/${COLOR:-silver}/g" ./app/views/layouts/application.html.erb
 echo "Success"
