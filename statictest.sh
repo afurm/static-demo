beec4710f20321448d5fad472637c610b781b15d
briannarelucio <briannarelucio@ge.com>
2018-07-10 16:51:03 -0700

    Tar

--- statictest.sh
+++ statictest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-
+ls
 cd ~
 tar -xvf artifact.tar && rm artifact.tar
 
@@ -7,4 +7,4 @@ ruby statictest.rb
 sed -e "s/COLOR_REPLACE_ME/${COLOR}/g" ./app/views/layouts/application.html.erb
 echo "Success"
 
-tar -cvf artifact.tar /Users/brianna/Desktop/staticdemo
+tar -cvf artifact.tar /usr/local/repos
