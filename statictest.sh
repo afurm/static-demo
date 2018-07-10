95b9c3689209ae63bde2d038ed696b4802af0b1f
briannarelucio <briannarelucio@ge.com>
2018-07-10 10:55:28 -0700

    Tar

--- statictest.sh
+++ statictest.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 ruby statictest.rb
-cd ~
-tar -xvf build_artifact.tar && rm build_artifact.tar
 sed -e "s/COLOR_REPLACE_ME/${COLOR}/g" ./app/views/layouts/application.html.erb
 echo "Success"
-tar -cvf scm_artifact.tar 
+tar -cvf artifact.tar 
