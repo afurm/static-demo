2bccf0f631aad7c0367ad98fc288d525bd4c02e1
briannarelucio <briannarelucio@ge.com>
2018-07-10 10:46:49 -0700

    Archiving files between steps

--- statictest.sh
+++ statictest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 ruby statictest.rb
+cd ~
+tar -xvf build_artifact.tar && rm build_artifact.tar
 sed -e "s/COLOR_REPLACE_ME/${COLOR}/g" ./app/views/layouts/application.html.erb
 echo "Success"
-
+tar -cvf scm_artifact.tar 
