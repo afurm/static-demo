6cf3c33f5efc79721cd903f49d2b51dd8c77e255
briannarelucio <briannarelucio@ge.com>
2018-07-13 09:43:21 -0700

    Tar

--- statictest.sh
+++ statictest.sh
@@ -9,5 +9,5 @@ sed -i "s/COLOR_REPLACE_ME/${COLOR}/g" ./app/views/layouts/application.html.erb
 # Tarball
 cd .. && ls -la  
 pwd static-demo
-tar -cvf change_artifact.tar ./repos/static-demo
+tar -cvf change_artifact.tar /usr/local/static-demo
 
