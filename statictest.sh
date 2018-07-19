3fba0af3aec3c8cb0f2b9174671d402a9c691f90
briannarelucio <briannarelucio@ge.com>
2018-07-19 13:00:45 -0700

    More changes

--- statictest.sh
+++ statictest.sh
@@ -5,6 +5,8 @@ ruby statictest.rb
 
 # Replace the color placeholder with the environment variable.
 sed -i "s/COLOR_REPLACE_ME/${COLOR}/g" ./app/views/layouts/application.html.erb
+currentDate='date'
+sed -i "s/Demo/$currentDate/g" ./app/views/layouts/application.html.erb
 
 # Tarball
 tar -cvf change_artifact.tar /usr/local/repos
