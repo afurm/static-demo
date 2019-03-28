dfe5db83162b829aab015af9da96458775a3dc9f
Andrii Furmanets <furmanets.andriy@gmail.com>
2019-03-28 16:24:09 +0200

    Update statictest.sh

--- statictest.sh
+++ statictest.sh
@@ -3,10 +3,12 @@
 # Unit test that always passes.
 ruby statictest.rb
 
-# # Replace the color placeholder with the environment variable.
-# sed -i "s/COLOR_REPLACE_ME/${COLOR}/g" ./app/views/layouts/application.html.erb
-# sed -i "s/Demo/$(date '+%m-%d-%Y')/g" ./app/views/layouts/application.html.erb
+# Replace the color placeholder with the environment variable.
+sed -i "s/COLOR_REPLACE_ME/${COLOR}/g" ./app/views/layouts/application.html.erb
+sed -i "s/Demo/$(date '+%m-%d-%Y')/g" ./app/views/layouts/application.html.erb
 
-# # Tarball
-tar -cvf change_artifact.tar /usr/local/repos
-# cp change_artifact.tar /root
+cd /home/predixci
+mkdir change_artifact
+cd change_artifact
+touch clone.txt
+
